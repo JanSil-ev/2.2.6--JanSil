0ff9929f02ef8b31117caca98190c3c44f9fbe1e
Silantiev Jan <Silantiev-999@mail.ru>
2025-08-22 14:49:25 +0300

    relis3

--- src/Hooks/useLocalStorage.tsx
+++ src/Hooks/useLocalStorage.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
-export function useLocalStorage(key: string, initialValue: any) {
-  const [value, setValue] = useState(initialValue);
+export function useLocalStorage(key: string, initialValue : any) {
+  const [value, setValue] = useState(() => {
+
+    const storedValue = localStorage.getItem(key);
+    return storedValue !== null ? storedValue : initialValue;
+  });
 
   const setStoredValue = (newValue: any) => {
     setValue(newValue);
-    localStorage.setItem(key, String(newValue));
+    localStorage.setItem(key, newValue);
   };
 
   return [value, setStoredValue];
